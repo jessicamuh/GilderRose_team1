75b337ca4c0c049fde38dcc5508f2bd9cd290c15
frlowe <flowe2@kennesaw.edu>
2022-02-22 19:30:44 -0500

    Team1 fix junit noclassfound exceptions

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,8 +1,8 @@
 package com.gildedrose;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 class GildedRoseTest {
 
